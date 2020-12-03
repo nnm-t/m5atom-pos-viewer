2bf0a16271509c19ce2c9cc6069129b7df0fdbe8
Noname Kamisawa <noname@kamisawa.net>
2020-12-03 23:57:23 +0900

    BLEClient処理 BLEPosClientクラスへ記述

--- m5atom-pos-viewer.ino
+++ m5atom-pos-viewer.ino
@@ -1,8 +1,10 @@
 #include "Arduino.h"
 #include "M5Atom.h"
+#include "BLEDevice.h"
 #include "TM1637Display.h"
 
 #include "matrix_number.h"
+#include "ble-pos-client.h"
 
 namespace
 {
@@ -10,10 +12,21 @@ namespace
     constexpr uint8_t clock_pin = 32;
     constexpr uint8_t brightness = 10;
 
+    BLEUUID service_uuid(static_cast<uint16_t>(0xFFFF));
+    BLEUUID num_characteristic_uuid(static_cast<uint16_t>(0x0000));
+    BLEUUID price_characteristic_uuid(static_cast<uint16_t>(0x0001));
+
+    BLEPosClient ble_client("M5Atom-Pos-Viewer", service_uuid, num_characteristic_uuid, price_characteristic_uuid);
+
     uint8_t count = 0;
 
     TM1637Display tm1637(clock_pin, dio_pin);
 }
+
+
+bool do_connect = false;
+bool do_scan = false;
+bool is_connected = false;
 
 bool drawNumbers(const uint8_t number)
 {
@@ -39,6 +52,8 @@ void setup()
 {
     M5.begin(true, true, true);
 
+    ble_client.Begin();
+
     M5.dis.setBrightness(brightness);
     tm1637.setBrightness(brightness);
     drawNumbers(count);
@@ -57,4 +72,6 @@ void loop()
 
         drawNumbers(count);
     }
+
+    ble_client.Update();
 }
